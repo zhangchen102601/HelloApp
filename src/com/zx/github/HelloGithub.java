5794b52daddc2493e066a88a1e429062f6d2e77c
zhangchen <zz1010659703@163.com>
2016-09-12 17:27:33 +0800

    test git update

--- src/com/zx/github/HelloGithub.java
+++ src/com/zx/github/HelloGithub.java
@@ -5,6 +5,8 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		System.out.println("Hello ,Github !");
 		System.out.println("Hello ,Github !");
+		
+		System.out.println("Oh,my God ,is ok git change!");
 	}
 
 }
